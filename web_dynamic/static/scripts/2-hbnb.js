60eeae924b58806b7592dfc992952ef033f2b3f0
ZidiEya <3805@holberton.com>
2022-07-12 03:19:36 +0000

    Update the API entry point (api/v1/app.py) by replacing the current CORS CORS(app, origins=0.0.0.0) by CORS(app, resources={r/api/v1/*: {origins: *}}).

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+// Import the JavaScript static/scripts/2-hbnb.js in the <head> tag (instead of 1-hbnb.js)
+document.addEventListener('DOMContentLoaded', () => {
+    const checked_list = {};
+    const checkbox = document.querySelectorAll('input[type=checkbox]').forEach(function (checks) {
+        checks.addEventListener("change", function () {
+            if (this.checked) {
+                checked_list[checks.getAttribute('data-id')] = checks.getAttribute('data-name');
+            }
+            else {
+                delete checked_list[checks.getAttribute('data-id')];
+            }
+            document.querySelector(".amenities h4").textContent = Object.values(checked_list).join(', ');
+        });
+    });
+
+    async function apiStatus(){
+        const response = await fetch("http://localhost:5001/api/v1/status");
+        const data = await response.json();
+        if (data.status !== 'OK') {
+            let elemento = document.getElementById('api_status')
+            elemento.className = ''
+        }
+    }
+    apiStatus();
+});
